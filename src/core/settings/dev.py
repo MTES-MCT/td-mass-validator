a816933207f0a9cf6659da04c9befb01c3433958
Laurent Paoletti <lp@providenz.fr>
2022-02-03 09:43:00 +0100

    Add email settings

--- src/core/settings/dev.py
+++ src/core/settings/dev.py
@@ -5,3 +5,4 @@ DEBUG = True
 SECRET_KEY = "xyzabcdefghu"
 
 INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
